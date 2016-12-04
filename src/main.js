877543920652396cfae059ff3120cff04b38b8cd
Suphakiat Lohasammakul <suphakiat.loh@ascendcorp.com>
2016-12-04 20:50:22 +0700

    Update package.json and main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
+
+import '!style!css!bootstrap/dist/css/bootstrap.min.css'
+import '!style!css!font-awesome/css/font-awesome.min.css'
+
+import '!script!jquery/dist/jquery.min.js'
+import '!script!bootstrap/dist/js/bootstrap.min.js'
+
 import App from './App'
 
 /* eslint-disable no-new */
